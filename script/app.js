2c05c9365ecf16d97af8807dbbb73a3c2550630c
utshabp <utsv.paudel23@gmail.com>
2022-02-06 20:03:17 +0545

    first commit

---
+++ script/app.js
@@ -0,0 +1,13 @@
+let ball=document.querySelector('.bouncingBall'),
+    y=1,
+    gravity=1;
+
+    setInterval(function(){
+        if(y>250)
+        gravity=gravity;
+        gravity+=1;
+        y+=gravity;
+        ball.style.top=(y)+'px';
+        
+
+    },100);
